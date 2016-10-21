37839457975d98152c30eba285f11fa6cf57ba80
Charlie Harvey <charlie@mallory.hendred.net>
2016-10-21 17:28:09 +0100

    More work stubbing out the Db interactions

--- src/Db.hs
+++ src/Db.hs
@@ -17,10 +17,12 @@ import Control.Monad.IO.Class (liftIO)
 import Data.Time
 import qualified Data.Text as T
 
-share [mkPersist sqlSettings, mkSave "entityDefs"] [persistLowerCase|
+share [mkPersist sqlSettings, mkMigrate "migrateAll", mkSave "entityDefs"] [persistLowerCase|
 Feed
     uri T.Text
     title T.Text
+    prepend T.Text
+    append T.Text
     tweetsPerRun Int
     checkEvery Int
     nextCheck UTCTime
@@ -35,13 +37,41 @@ Entry
     deriving Show
 |]
 
+feedsToUpdate = do
+    now <- liftIO getCurrentTime
+    fs <- selectList [FeedNextCheck <=. now] []
+    return $ map entityVal fs
+
+doMigrations =
+    runMigration $ migrate entityDefs $ entityDef (Nothing :: Maybe Feed)
+
+-- createFeed (Feed ...) - create new feed, Should be able to touch from CLI
+
+-- updateFeed
+--   -- get entries from db
+--   -- if more than 0
+--   --    -- They need tweeting (Twitter.tweetEntry)
+--   -- otherwise
+--   --    -- Get the feed from the internet (FetchFeed.fetchFeed)
+--   --    -- add any new entries (insertEntries)
+--   --    -- tweet feed.tweetsPerRun entries (and mark as tweeted)
+
+-- updateFeeds
+
+-- createEntries  = map createEntry
+
+-- createEntry (Entry ...) - create new entry
+
+-- This should be somewhere else!
 main :: IO ()
 main = runSqlite ":memory:" $ do
-    -- this line added: that's it!
-    runMigration $ migrate entityDefs $ entityDef (Nothing :: Maybe Feed)
-    michaelId <- insert $ Feed "http://charlieharvey.org.uk/page/feed/rss" "Charlie RSS" 
-                               1
-                               30
-                               (UTCTime (fromGregorian 2016 10 21) 1200)
-    michael <- get michaelId
-    liftIO $ print michael
+    doMigrations
+    fid <- insert $ Feed "http://charlieharvey.org.uk/page/feed/rss" 
+                         "Charlie RSS"
+                         ""
+                         " #charlieharvey"
+                         1
+                         30
+                         (UTCTime (fromGregorian 2016 10 20) 1200)
+    feeds <- feedsToUpdate
+    liftIO $ print feeds
